6004a249b59250708cf8903eac6cb504ab1a212e
Ryan Yacyshyn <ryan.yacyshyn@gmail.com>
2015-05-25 18:37:46 +0800

    autosuggest with h2s linking to mom's dev site..

--- public/coffee/main.coffee
+++ public/coffee/main.coffee
@@ -1,16 +1,50 @@
 $ ->
   console.log 'ready..'
 
+  baseUrl = 'http://momcorp.internet.dynawebsite.com'
+
+  # adapted from: https://github.com/PebbleRoad/mom/blob/master/parser/shortcodes.mapping.php#L251-L261
+  pr_sanitize = (hash) ->
+
+    # lower case everything
+    str = hash.toLowerCase()
+    # remove solr's <em> tags..
+    str = str.replace(/\<(\/?)em\>/gi, '')
+    # make alphanumeric (removes all other characters)
+    str = str.replace(/[^a-z0-9_\s-]/gi, '')
+    # clean up multiple dashes or whitespaces
+    str = str.replace(/[\s-]+/gi, ' ')
+    # convert whitespaces and underscore to dash
+    str = str.replace(/[\s_]/gi, '-')
+
+    return '#' + str
+
   getSuggestions = ->
     (q, cb) ->
       matches = []
       $.get 'http://localhost:8983/solr/mom/autosuggest', { q: q }, (res) ->
+
         data = JSON.parse res
-        console.log data.response.docs
-        cb data.response.docs
+        docs = data.response.docs
+        
+        autosuggestData = $.map docs, (item) ->
+          doc = { value: item.value }
+          doc.subTitle = data.highlighting[item.id].subTitles[0] if data.highlighting[item.id].subTitles?
+          doc.url = baseUrl + item.url + pr_sanitize(doc.subTitle) if doc.subTitle?
+          return doc
+
+        cb autosuggestData
 
   init = ->
     console.log 'init..'
-    $('.typeahead').typeahead { minLength: 2, hint: true, highlight: true }, { name: 'pages', source: getSuggestions(), display: 'value' }
+    opts = {
+      name: 'pages'
+      source: getSuggestions()
+      display: 'value'
+      templates: {
+        suggestion: Handlebars.compile('<div class="autosuggest-item">{{value}}<br/><a class="subtitle" href="{{url}}">{{{subTitle}}}</a></div>')
+      }
+    }
+    $('.typeahead').typeahead { minLength: 2, hint: true, highlight: true }, opts
 
   init()
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
 $(function() {
-  var getSuggestions, init;
+  var baseUrl, getSuggestions, init, pr_sanitize;
   console.log('ready..');
+  baseUrl = 'http://momcorp.internet.dynawebsite.com';
+  pr_sanitize = function(hash) {
+    var str;
+    str = hash.toLowerCase();
+    str = str.replace(/\<(\/?)em\>/gi, '');
+    str = str.replace(/[^a-z0-9_\s-]/gi, '');
+    str = str.replace(/[\s-]+/gi, ' ');
+    str = str.replace(/[\s_]/gi, '-');
+    return '#' + str;
+  };
   getSuggestions = function() {
     return function(q, cb) {
       var matches;
@@ -8,24 +18,42 @@ $(function() {
       return $.get('http://localhost:8983/solr/mom/autosuggest', {
         q: q
       }, function(res) {
-        var data;
+        var autosuggestData, data, docs;
         data = JSON.parse(res);
-        console.log(data.response.docs);
-        return cb(data.response.docs);
+        docs = data.response.docs;
+        autosuggestData = $.map(docs, function(item) {
+          var doc;
+          doc = {
+            value: item.value
+          };
+          if (data.highlighting[item.id].subTitles != null) {
+            doc.subTitle = data.highlighting[item.id].subTitles[0];
+          }
+          if (doc.subTitle != null) {
+            doc.url = baseUrl + item.url + pr_sanitize(doc.subTitle);
+          }
+          return doc;
+        });
+        return cb(autosuggestData);
       });
     };
   };
   init = function() {
+    var opts;
     console.log('init..');
+    opts = {
+      name: 'pages',
+      source: getSuggestions(),
+      display: 'value',
+      templates: {
+        suggestion: Handlebars.compile('<div class="autosuggest-item">{{value}}<br/><a class="subtitle" href="{{url}}">{{{subTitle}}}</a></div>')
+      }
+    };
     return $('.typeahead').typeahead({
       minLength: 2,
       hint: true,
       highlight: true
-    }, {
-      name: 'pages',
-      source: getSuggestions(),
-      display: 'value'
-    });
+    }, opts);
   };
   return init();
 });
